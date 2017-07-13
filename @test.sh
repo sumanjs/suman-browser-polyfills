ef3e1b99907b26a31a66f073399f20c4ab8e4564
Olegzandr <alex@oresoftware.com>
2017-07-13 16:02:10 -0700

    pdev:set

--- @test.sh
+++ @test.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
+
+which_suman=$(which suman);
+
+if [[ -z ${which_suman} ]]; then
+    npm install -g github:sumanjs/suman#rebase_branch
+fi
+
 echo "not yet implemented";
 exit 1;
